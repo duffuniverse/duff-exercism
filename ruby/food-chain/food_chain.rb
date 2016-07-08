3c9acdcb3bb6fa3a8dbcc63e9cee6ca4511a6254
Andrew Sobolev <andrewsobolev@inbox.ru>
2016-07-08 12:46:21 +0300

    Solve food chain problem. It was an amazing night trip!

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -6,52 +6,95 @@ end
 
 module FoodChain
   CHARACTERS = [
-    OpenStruct.new(:name => "fly"),
-    OpenStruct.new(:name => "spider"),
-    OpenStruct.new(:name => "bird"),
-    OpenStruct.new(:name => "cat"),
-    OpenStruct.new(:name => "dog"),
-    OpenStruct.new(:name => "goat"),
-    OpenStruct.new(:name => "cow"),
-    OpenStruct.new(:name => "horse")
+    OpenStruct.new(
+      :name => "fly", 
+      remark: "",
+      action: ""
+    ),
+    OpenStruct.new(
+      :name => "spider", 
+      remark: "It wriggled and jiggled and tickled inside her.",
+      action: " that wriggled and jiggled and tickled inside her"
+    ),
+    OpenStruct.new(
+      :name => "bird", 
+      remark: "How absurd to swallow a bird!",
+      action: ""
+    ),
+    OpenStruct.new(
+      :name => "cat", 
+      remark: "Imagine that, to swallow a cat!",
+      action: ""
+    ),
+    OpenStruct.new(
+      :name => "dog", 
+      remark: "What a hog, to swallow a dog!",
+      action: ""
+    ),
+    OpenStruct.new(
+      :name => "goat", 
+      remark: "Just opened her throat and swallowed a goat!",
+      action: ""
+    ),
+    OpenStruct.new(
+      :name => "cow", 
+      remark: "I don't know how she swallowed a cow!",
+      action: ""
+    ),
+    OpenStruct.new(
+      :name => "horse", 
+      remark: "",
+      action: ""
+    )
   ]
 
   def self.song
-    (0...CHARACTERS.length).each_with_object([]) { |n, stories|
-      stories << Story.new(CHARACTERS[0..n]).tell
-    }.join("\n\n")
+    end_index = CHARACTERS.length - 1
+
+    (0..end_index).each_with_object([]) { |n, cumulative_song|
+      cumulative_song << Story.new(CHARACTERS[0..n], n == end_index).tell
+    }.join("\n\n").concat("\n")
   end
 end
 
 class Story
-  attr_reader :characters
+  attr_reader :characters, :final
 
-  def initialize(characters)
+  def initialize(characters, final)
     @characters = characters
+    @final = final
   end
 
   def tell
-    [intro, history, outro].reject { |piece|
-      piece.empty? 
-    }.join("\n")
+    [intro, remark, history, outro].reject { |part| part.empty? }.join("\n")
   end
 
   private
 
+    def first_in_chain
+      characters.first
+    end 
+
+    def last_in_chain
+      characters.last
+    end
+
     def intro
-      "I know an old lady who swallowed a #{characters.last.name}."
+      "I know an old lady who swallowed a #{last_in_chain.name}."
     end
 
     def remark
-      characters.last.remark
+      last_in_chain.remark
     end
 
     def outro
-      "I don't know why she swallowed the #{characters.first.name}. Perhaps she'll die."
+      return "She's dead, of course!" if final
+
+      "I don't know why she swallowed the #{first_in_chain.name}. Perhaps she'll die."
     end
 
     def history
-      return "" unless characters.length > 1
+      return "" if characters.length <= 1 || final 
 
       records = []
       characters.each_cons(2) do |previous, current| 
@@ -61,8 +104,6 @@ class Story
     end
 
     def record(victim, attacker)
-      "She swallowed the #{attacker.name} to catch the #{victim.name}."
+      "She swallowed the #{attacker.name} to catch the #{victim.name}#{victim.action}."
     end
 end
-
-puts FoodChain.song
