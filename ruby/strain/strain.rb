6b268a7b761586bd3aca3778280ce413329351bb
Andrew Sobolev <andrewsobolev@inbox.ru>
2016-07-29 22:20:21 +0300

    Complete Strain problem solution

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,37 +1,9 @@
 module Enumerable
   def keep
-    return to_enum(__method__) unless block_given?
-    inject([]) do |memo, element| 
-      memo << element if yield element 
-      memo
-    end
+    each_with_object([]) { |element, memo| memo << element if yield(element) }
   end
 
   def discard
-    return to_enum(__method__) unless block_given?
-    inject([]) do |memo, element| 
-      memo << element unless yield element 
-      memo
-    end
+    each_with_object([]) { |element, memo| memo << element unless yield(element) }
   end
 end
-
-=begin
-# Awesome method_missing solution
-module Strain
-  def method_missing(m, *args, &blk)
-    case m
-    when :keep, :discard
-      partition {|e| e if yield(e)}[m == :keep && 0 || 1]
-    else
-      super
-    end
-  end
-
-  def respond_to_missing?(*arg)
-    [:keep, :discard].include? arg[0]
-  end
-end
-
-include Strain
-=end
