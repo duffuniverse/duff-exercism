944fcc82e82f2dcad9e95c36db8f6ce69d9be91b
Andrew Sobolev <andrewsobolev@inbox.ru>
2016-09-04 16:25:43 +0300

    Refactor scrabble-score code

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,29 +1,23 @@
 class Scrabble
-  DEFAULT_SCORE = 0
   LETTER_VALUES = {
-    ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
-    ["D", "G"] => 2,
-    ["B", "C", "M", "P"] => 3,
-    ["F", "H", "V", "W", "Y"] => 4,
-    ["K"] => 5,
-    ["J", "X"] => 8,
-    ["Q", "Z"] => 10
+    a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1,
+    d: 2, g: 2,
+    b: 3, c: 3, m: 3, p: 3,
+    f: 4, h: 4, v: 4, w: 4, y: 4,
+    k: 5,
+    j: 8, x: 8,
+    q: 10, z: 10
   }.freeze
 
   def self.score(word)
-    return DEFAULT_SCORE unless word
-
     word.
-      upcase.
+      to_s.
+      strip.
+      downcase.
       chars.
-      map do |char| 
-        LETTER_VALUES.keys.find do |key|
-          key.include?(char)
-        end
-      end.
-      compact.
+      map(&:to_sym).
       map { |key| LETTER_VALUES[key] }.
-      reduce(DEFAULT_SCORE, :+)
+      inject(0, :+)
   end
 
   def initialize(word)
