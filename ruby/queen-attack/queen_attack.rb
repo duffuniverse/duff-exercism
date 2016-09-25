e20e9180f3bc91a37020faaf20caba4608ee1f84
Andrew Sobolev <andrewsobolev@inbox.ru>
2016-09-25 18:48:19 +0300

    Refactor queen attack code.

--- ruby/queen-attack/queen_attack.rb
+++ ruby/queen-attack/queen_attack.rb
@@ -1,53 +1,38 @@
 class Queens
-  DEFAULT_POSITIONS = {
-    white: [0, 3],
-    black: [7, 3]
-  }.freeze
+  attr_reader :white, :black
 
-  BOARD_SIZE = 8
-
-  def initialize(positions = nil)
-    @positions = positions || DEFAULT_POSITIONS
+  def initialize(white: [0, 3], black: [7, 3])
     raise ArgumentError if white == black
+
+    @white = white
+    @black = black
   end
 
   def to_s
-    (0...BOARD_SIZE).to_a.
-      repeated_permutation(2).
-      inject([]) { |board, position| board << piece(position) }.
-      each_slice(BOARD_SIZE).
-      inject([]) { |result, batch| result << batch.join(" ") }.
-      join("\n")
+    Array.new(8) { Array.new(8) { "_" } }.
+      tap do |board|
+        board[white[0]][white[1]] = 'W'
+        board[black[0]][black[1]] = 'B'
+    end.
+    map { |row| row.join(" ") }.
+    join("\n")
   end
 
   def attack?
-    on_same_horizontal? || on_same_vertical? || on_same_diagonal?
-  end
-
-  DEFAULT_POSITIONS.keys.each do |queen_color|
-    define_method(queen_color) { @positions[queen_color] }
+    on_horizontal? || on_vertical? || on_diagonal?
   end
 
   private
 
-  def piece(position)
-    case position
-    when white then "W"
-    when black then "B"
-    else
-      "_"
-    end
-  end
-
-  def on_same_horizontal?
+  def on_horizontal?
     white[0] == black[0]
   end
 
-  def on_same_vertical?
+  def on_vertical?
     white[1] == black[1]
   end
 
-  def on_same_diagonal?
+  def on_diagonal?
     (white[1] - black[1]).abs == (white[0] - black[0]).abs
   end
 end
